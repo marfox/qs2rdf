42f0f1f3f4cbe088db10dfcd4b68f3e0bc124d49
Marco Fossati <fossati@spaziodati.eu>
2017-10-24 18:32:28 +0200

    command line support

--- qs2rdf.py
+++ qs2rdf.py
@@ -3,12 +3,15 @@
 
 import logging
 import re
+import click
 from rdflib import Graph, Namespace, URIRef, Literal, XSD
 from uuid import uuid4 # Random UUIDs for statement nodes
 from hashlib import sha1 # SHA-1 hashes for reference nodes
 from urllib.parse import urlparse
 from sys import exit, argv
 
+
+SHA = sha1()
 
 # Minimal set of Wikidata namespaces
 BASE_URI = 'http://www.wikidata.org/'
@@ -22,8 +25,6 @@ PR = Namespace(P + 'reference/')
 PROV = Namespace('http://www.w3.org/ns/prov#')
 GEO = Namespace('http://www.opengis.net/ont/geosparql#')
 
-SHA = sha1()
-
 # Value data types matchers
 ITEM = re.compile(r'^Q\d+$')
 PROPERTY = re.compile(r'^P\d+$')
@@ -33,25 +34,22 @@ QUANTITY = re.compile(r'^[+-]\d+(\.\d+)?$')
 MONOLINGUAL_TEXT = re.compile(r'^(\w+):("[^"\\]*(?:\\.[^"\\]*)*")$')
 
 
-def setup_logger(level='debug', log_filename='conversion.log'):
+def setup_logger(level, log_filename):
     """ Convenience function to setup proper logging """
     levels = {'info': logging.INFO, 'warning': logging.WARNING, 'debug': logging.DEBUG}
     logger = logging.getLogger(__name__)
     logger.setLevel(levels[level])
-    # Message format
     logFormatter = logging.Formatter("%(asctime)s [%(levelname)s] %(module)s.%(funcName)s #%(lineno)d - %(message)s")
-    # Log to file
-    fileHandler = logging.FileHandler(log_filename)
-    fileHandler.setFormatter(logFormatter)
-    logger.addHandler(fileHandler)
-    # Log to console
+    # Always log to console
     consoleHandler = logging.StreamHandler()
     consoleHandler.setFormatter(logFormatter)
     logger.addHandler(consoleHandler)
+    # Log to file if given
+    if log_filename:
+        fileHandler = logging.FileHandler(log_filename)
+        fileHandler.setFormatter(logFormatter)
+        logger.addHandler(fileHandler)
     return logger
-
-
-logger = setup_logger()
 
 
 def setup_prefixes(graph):
@@ -68,36 +66,34 @@ def setup_prefixes(graph):
     return
 
 
-def convert(fin, fout='out.ttl'):
+def convert(fin, fout, logger):
     """ Convert a QuickStatements dataset into a Wikidata RDF one """
     g = Graph()
     setup_prefixes(g)
-    with open(fin) as i:
-        for statement in i:
-            elements = statement.strip().split('\t')
-            logger.debug(elements)
-            subject = elements[0]
-            main_pid = elements[1]
-            value = handle_value(elements[2])
-            st_node = mint_statement_node(subject)
-            g.add((URIRef(WD + subject), URIRef(P + main_pid), st_node))
-            g.add((st_node, URIRef(PS + main_pid), value))
-            quals_and_refs = zip(*[iter(elements[3:])] * 2)
-            for (prop, val) in quals_and_refs:
-                val = handle_value(val)
-                if prop.startswith('S'):
-                    ref_node = mint_reference_node(val)
-                    g.add((st_node, PROV.wasDerivedFrom, ref_node))
-                    g.add((ref_node, PR.prop, val))
-                else:
-                    g.add((st_node, PQ.prop, val))
-    with open(fout, 'w') as o:
-        # TODO rdlifb seems to randomly ignore some prefixes
-        g.serialize(destination=fout, format='turtle')
+    for statement in fin:
+        elements = statement.strip().split('\t')
+        logger.debug(elements)
+        subject = elements[0]
+        main_pid = elements[1]
+        value = handle_value(elements[2], logger)
+        st_node = mint_statement_node(subject)
+        g.add((URIRef(WD + subject), URIRef(P + main_pid), st_node))
+        g.add((st_node, URIRef(PS + main_pid), value))
+        quals_and_refs = zip(*[iter(elements[3:])] * 2)
+        for (prop, val) in quals_and_refs:
+            val = handle_value(val, logger)
+            if prop.startswith('S'):
+                ref_node = mint_reference_node(val)
+                g.add((st_node, PROV.wasDerivedFrom, ref_node))
+                g.add((ref_node, PR.prop, val))
+            else:
+                g.add((st_node, PQ.prop, val))
+    # TODO rdlifb seems to randomly ignore some prefixes
+    g.serialize(destination=fout, format='turtle')
     return
 
 
-def handle_value(value):
+def handle_value(value, logger):
     """
     Handle value data types.
     See https://www.wikidata.org/wiki/Help:QuickStatements#Command_sequence_syntax
@@ -107,13 +103,13 @@ def handle_value(value):
         item = URIRef(WD + value)
         logger.debug('Item. From [%s] to [%s]' % (value, item.n3()))
         return item
-    # monolingual text
+    # Monolingual text
     elif MONOLINGUAL_TEXT.match(value):
         match = MONOLINGUAL_TEXT.match(value)
         literal = Literal(match.group(2).strip('"'), lang=match.group(1))
         logger.debug('Monolingual text. From [%s] to [%s]' % (value, literal.n3()))
         return literal
-    # time YYYY-MM-DDThh:mm:ssZ
+    # Time YYYY-MM-DDThh:mm:ssZ
     elif TIME.match(value):
         # TODO handle precision via a complex RDF value
         time, precision = value.split('/')
@@ -125,7 +121,7 @@ def handle_value(value):
         time = Literal(time[0] + year + '-' + rest, datatype=XSD.dateTime)
         logger.debug('Time. From [%s] to [%s]' % (value, time.n3()))
         return time
-    # location
+    # Location
     elif LOCATION.match(value):
         match = LOCATION.match(value)
         lat = match.group(1)
@@ -134,7 +130,7 @@ def handle_value(value):
         point = Literal('Point(%s %s)' % (lon, lat), datatype=GEO.wktLiteral)
         logger.debug('Location. From [%s] to [%s]' (value, point.n3()))
         return point
-    # quantity
+    # Quantity
     elif QUANTITY.match(value):
         quantity = Literal(value, datatype=XSD.decimal)
         logger.debug('Quantity. From [%s] to [%s]' % (value, quantity.n3()))
@@ -165,8 +161,14 @@ def mint_reference_node(reference_value):
     return URIRef(WDREF + SHA.hexdigest())
 
 
-def main():
-    convert(argv[1])
+@click.command()
+@click.argument('dataset', type=click.File())
+@click.option('--output', '-o', type=click.Path(dir_okay=False), default='output.ttl')
+@click.option('--debug', '-d', is_flag=True, default=False)
+@click.option('--logfile', '-l', type=click.File('w'), default=None)
+def main(dataset, output, debug, logfile):
+    logger = setup_logger('debug', logfile) if debug else setup_logger('info', logfile)
+    convert(dataset, output, logger)
     return 0
 
 
